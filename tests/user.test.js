da51bde1cce95ed33ac6b94438b6c5fa53fed61e
shakil <send2shakil@gmail.com>
2022-03-30 00:10:27 +0600

    init tests success

---
+++ tests/user.test.js
@@ -0,0 +1,26 @@
+require('dotenv').config({path:'./.test.env'})
+const request = require('supertest');
+const app = require("../index");
+const mongoose=require("mongoose")
+
+
+beforeAll(async() => {
+    //connects to the database
+    await mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    // Clears the database and adds some testing data.
+    const collections = await mongoose.connection.db.listCollections().toArray()
+    if(collections.includes('users')){
+        console.log("has user")
+        return mongoose.connection.db.dropCollection('users');
+    }else{
+        console.log("no user")
+        return
+    }
+    
+  });
+
+// beforeEach(populateDB)
+
+test('register user', async ()=>{
+    expect(1==1)
+});
