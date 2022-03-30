4b248929b6631fe0048a5f82cdf5691063ec43df
shakil <send2shakil@gmail.com>
2022-03-30 13:07:04 +0600

    refractored before all added a couple of test suite

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const port = process.env.PORT || 3000;
 
 //importing routers
 const userRouter = require('./routers/user');
-
+const productRouter = require('./routers/products');
 //config
 app.use(express.json())
 // using the "user" router
 app.use('/user', userRouter);
-
+app.use('/product', productRouter);
 
 app.use(express.json())
 
