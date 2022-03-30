d4f95f3e1c745d1ca43b58cf19688decf45d993d
shakil <send2shakil@gmail.com>
2022-03-31 00:01:52 +0600

    fixed add product route

--- controllers/products/add.js
+++ controllers/products/add.js
@@ -2,14 +2,15 @@ const Product = require("../../Models/Product");
 
 module.exports = async (req, res) => {
   try {
-    const products = new Product({
+    const product = new Product({
       name: req.body.name,
       categoryId: req.body.categoryId,
       categoryName: req.body.categoryName,
       unitPrice: req.body.unitPrice,
       status: req.body.status,
     });
-    res.status(201).send(products);
+    await product.save()
+    res.status(201).send(product);
   } catch (e) {
     console.log(e);
     res.status(500).send(e);
