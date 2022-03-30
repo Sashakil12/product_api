fedce99bd1f25c29b6da9c950ffb39886659c8cd
shakil <send2shakil@gmail.com>
2022-03-30 16:39:42 +0600

    added validations

---
+++ controllers/products/add.js
@@ -0,0 +1,17 @@
+const Product = require("../../Models/Product");
+
+module.exports = async (req, res) => {
+  try {
+    const products = new Product({
+      name: req.body.name,
+      categoryId: req.body.categoryId,
+      categoryName: req.body.categoryName,
+      unitPrice: req.body.unitPrice,
+      status: req.body.status,
+    });
+    res.send(products);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send(e);
+  }
+};
